32db288ad1ce92a222c8cc2b188862c66c2e62de
Mohit <me.mohitsharma14@gmail.com>
2024-06-11 07:01:39 +0530

    Dimond Pattern

---
+++ Pattern/Dimond Shape.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // Upper part of the diamond
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower part of the diamond
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
